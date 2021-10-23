810751243584ab2075da9d4e1d9f2eb6d0a15071
LuffyDrago <rigelvicky@gmail.com>
2021-10-23 15:59:58 +0300

    Build errors

--- src/components/ServicesCard.js
+++ src/components/ServicesCard.js
@@ -5,7 +5,13 @@ const ServicesCard = ({ img, title, description }) => {
     <>
       <div className="cards">
         <div className="cards-circle">
-          <img src={img} className="card__img" alt="description of image" />
+          <input
+            type="image"
+            img
+            src={img}
+            className="card__img"
+            alt="description of image"
+          />
         </div>
         <h3 className="cards-title">{title}</h3>
         <p className="cards-paragraph">{description}</p>
